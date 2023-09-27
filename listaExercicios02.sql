a4f596f23c3f769d81ba31b26f73d8e515d60081
Vinicius168 <102707503+Vinicius168@users.noreply.github.com>
2023-09-26 23:57:37 -0300

    Update listaExercicios02.sql
    exercicio 5

--- listaExercicios02.sql
+++ listaExercicios02.sql
@@ -52,3 +52,15 @@ delimiter ;
 call sp_VerificarLivrosCategoria('História', @possui_livros);
 select @possui_livros;
 drop procedure sp_VerificarLivrosCategoria;
+
+--exercicio 5
+CREATE PROCEDURE sp_LivrosAteAno(IN anoPublicacao INT)
+BEGIN
+    SELECT Livro.Titulo, Livro.Ano_Publicacao, Autor.Nome, Autor.Sobrenome
+    FROM Livro
+    INNER JOIN Autor_Livro ON Livro.Livro_ID = Autor_Livro.Livro_ID
+    INNER JOIN Autor ON Autor_Livro.Autor_ID = Autor.Autor_ID
+    WHERE Livro.Ano_Publicacao <= anoPublicacao;
+END;
+
+//
